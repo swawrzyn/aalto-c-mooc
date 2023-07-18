fe5b64b619cf745934338c896e1d1c139bc6d877
Stefan Wawrzyn <swawrzyn@gmail.com>
2023-07-18 16:33:10 +0800

    return helper function distance

--- exercises/Module_1/07_geometry/src/source.c
+++ exercises/Module_1/07_geometry/src/source.c
@@ -2,7 +2,6 @@
 #include <math.h>
 #include "source.h"
 
-
 void multi_table(unsigned int xsize, unsigned int ysize)
 {
 }
@@ -13,8 +12,8 @@ void draw_triangle(unsigned int size)
 
 double distance(int x, int y)
 {
+    return sqrt(x * x + y * y);
 }
-
 
 void draw_ball(unsigned int radius)
 {
