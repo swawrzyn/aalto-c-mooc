80df9f0443dd977839505320745455afa0911057
Stefan Wawrzyn <swawrzyn@gmail.com>
2023-07-18 16:32:48 +0800

    save 07_geometry progress

--- exercises/Module_1/05_calc/src/source.c
+++ exercises/Module_1/05_calc/src/source.c
@@ -4,8 +4,43 @@
 
 void simple_sum(void)
 {
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    printf("%d + %d = %d\n", a, b, a + b);
 }
 
 void simple_math(void)
 {
+    double a, b;
+    int ret;
+    char op;
+
+    ret = scanf("%lf %c %lf", &a, &op, &b);
+
+    if (ret < 3)
+    {
+        printf("ERR\n");
+    }
+    else if (op == '+')
+    {
+        printf("%.1f\n", a + b);
+    }
+    else if (op == '-')
+    {
+        printf("%.1f\n", a - b);
+    }
+    else if (op == '*')
+    {
+        printf("%.1f\n", a * b);
+    }
+    else if (op == '/')
+    {
+        printf("%.1f\n", a / b);
+    }
+    else
+    {
+        printf("ERR\n");
+    }
 }
--- exercises/Module_1/07_geometry/src/source.c
+++ exercises/Module_1/07_geometry/src/source.c
@@ -4,10 +4,37 @@
 
 void multi_table(unsigned int xsize, unsigned int ysize)
 {
+    unsigned int x, y;
+
+    for (y = 1; y < ysize; y++)
+    {
+        for (x = 1; x < xsize; x++)
+        {
+            printf("%4u", x * y);
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 
 void draw_triangle(unsigned int size)
 {
+    unsigned int s;
+
+    for (s = 1; s <= size; s++) {
+        unsigned int dots, hashes;
+        for (dots = size - s; dots > 0; dots--) {
+            printf(".");
+        }
+
+        for (hashes = s; hashes > 0; hashes--) {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+
+    printf("\n");
 }
 
 double distance(int x, int y)
