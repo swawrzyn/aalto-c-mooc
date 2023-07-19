1461a0f4e769b29772b82b5c4bece4e2da4550e4
Stefan Wawrzyn <swawrzyn@gmail.com>
2023-07-19 13:29:50 +0800

    rebase fix

--- exercises/Module_1/08_characters/src/source.c
+++ exercises/Module_1/08_characters/src/source.c
@@ -1,8 +1,7 @@
-#include "source.h"
-
-#include <ctype.h>
 #include <stdio.h>
+#include <ctype.h>
 #include <string.h>
+#include "source.h"
 
 /* Don't touch the definition of msgs array! Checker uses this. */
 char *msgs[10] = { "'6=*w+~)._", "J65+~5+~=0/*69,~+9;,9*~19++=79" };
