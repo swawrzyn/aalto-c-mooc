fcbfa43829570df1c38b5291c1d3e9f1111a21bb
Stefan Wawrzyn <swawrzyn@gmail.com>
2023-07-19 11:28:13 +0800

    fix 08_characters init source functions

--- exercises/Module_1/08_characters/src/source.c
+++ exercises/Module_1/08_characters/src/source.c
@@ -8,17 +8,20 @@ char *msgs[10] = {
 	"'6=*w+~)._", "J65+~5+~=0/*69,~+9;,9*~19++=79"
 };
 
-void print_number(int number)
-{
-}
-
 void ascii_chart(char min, char max)
 {
 }
 
-char get_character(int msg, unsigned int cc)
-{
+char get_character(int msg, unsigned int cc) {
+    if (msg >= 10 || !msgs[msg])
+        return 0;
+
+    if (strlen(msgs[msg]) <= cc)
+        return 0;
+    
+    return msgs[msg][cc];
 }
+
 
 void secret_msg(int msg)
 {
