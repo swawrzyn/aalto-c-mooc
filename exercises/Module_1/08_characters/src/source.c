2c15360d07047af49bed532c45c5075005874eb1
Stefan Wawrzyn <swawrzyn@gmail.com>
2023-07-19 11:45:07 +0800

    complete 08_characters

--- exercises/Module_1/08_characters/src/source.c
+++ exercises/Module_1/08_characters/src/source.c
@@ -5,26 +5,27 @@
 #include <string.h>
 
 /* Don't touch the definition of msgs array! Checker uses this. */
-char *msgs[10] = {"'6=*w+~)._", "J65+~5+~=0/*69,~+9;,9*~19++=79"};
+char *msgs[10] = { "'6=*w+~)._", "J65+~5+~=0/*69,~+9;,9*~19++=79" };
 
-void ascii_chart(char min, char max) {
-  int i, j = 1;
+void ascii_chart(char min, char max)
+{
+	int i, j = 1;
 
-  for (i = min; i <= max; i++) {
-    if (isprint(i) != 0) {
-      printf("%3d 0x%2x %c", i, i, i);
-    } else {
-      printf("%3d 0x%2x ?", i, i);
-    }
+	for (i = min; i <= max; i++) {
+		if (isprint(i) != 0) {
+			printf("%3d 0x%2x %c", i, i, i);
+		} else {
+			printf("%3d 0x%2x ?", i, i);
+		}
 
-    if (j % 4 == 0) {
-      printf("\n");
-    } else {
-      printf("\t");
-    }
+		if (j % 4 == 0) {
+			printf("\n");
+		} else {
+			printf("\t");
+		}
 
-    j++;
-  }
+		j++;
+	}
 }
 
 char get_character(int msg, unsigned int cc)
@@ -40,4 +41,12 @@ char get_character(int msg, unsigned int cc)
 
 void secret_msg(int msg)
 {
+	int i = 0;
+	int j = get_character(msg, i);
+
+	while (j > 0) {
+		printf("%c", 158 - j);
+		i++;
+		j = get_character(msg, i);
+	}
 }
