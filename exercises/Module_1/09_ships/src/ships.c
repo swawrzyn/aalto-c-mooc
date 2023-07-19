7e5cec584744bbe3cfb06303f2fbf4d05cf0267d
Stefan Wawrzyn <swawrzyn@gmail.com>
2023-07-19 14:31:28 +0800

    finished module 1

--- exercises/Module_1/09_ships/src/ships.c
+++ exercises/Module_1/09_ships/src/ships.c
@@ -12,13 +12,38 @@ const unsigned int shiplen = 3;
  */
 void set_ships(unsigned int num)
 {
-	(void)num;
+	unsigned int i;
+
+	for (i = num; i > 0; i--) {
+		for (;;) {
+			unsigned int xloc = rand() % 10;
+			unsigned int yloc = rand() % 10;
+			// 0 = horiz, 1 = vert
+			unsigned int direction = rand() % 2;
+
+			if (place_ship(xloc, yloc, direction) == 1) {
+				break;
+			}
+		}
+	}
 }
 
 /* Task b: print the game field
  */
 void print_field(void)
 {
+	unsigned int x, y;
+
+	for (y = 0; y <= 9; y++) {
+		for (x = 0; x <= 9; x++) {
+			if (is_visible(x, y) == 1) {
+				printf("%c", is_ship(x, y));
+			} else {
+				printf("?");
+			}
+		}
+		printf("\n");
+	}
 }
 
 /* Task c: Ask coordinates (two integers) from user, and shoot the location.
@@ -27,7 +52,26 @@ void print_field(void)
  */
 int shoot(void)
 {
-	return -1; // replace this
+	unsigned int a, b, res;
+
+	res = scanf("%d %d", &a, &b);
+
+	if (res < 2 || a > 9 || b > 9) {
+		return -1;
+	}
+
+	checked(a, b);
+
+	char check_ship = is_ship(a, b);
+
+	if (check_ship == '#') {
+		return -1;
+	} else if (check_ship == '+') {
+		hit_ship(a, b);
+		return 1;
+	}
+
+	return 0;
 }
 
 /* Task d: Returns 1 if game is over (all ships are sunk), or 0 if there
@@ -37,6 +81,20 @@ int shoot(void)
  */
 int game_over(unsigned int num)
 {
-	(void)num;
-	return 0; // replace this
+	unsigned int x, y;
+	unsigned int hits = num * 3;
+
+	for (y = 0; y <= 9; y++) {
+		for (x = 0; x <= 9; x++) {
+			if (is_ship(x, y) == '#') {
+				hits--;
+			}
+		}
+	}
+
+	if (hits > 0) {
+		return 0;
+	}
+
+	return 1;
 }
